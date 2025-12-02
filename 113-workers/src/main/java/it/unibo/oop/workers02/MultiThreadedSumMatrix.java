6b756258283405be0f629663c3b4e7bdda9943a2
Chiara <chiara.emina@studio.unibo.it>
2025-12-02 15:27:33 +0100

    create MultiThreadedSumMatrix.java

---
+++ 113-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,14 @@
+package it.unibo.oop.workers02;
+
+/**
+ * This is a standard implementation of the calculation.
+ *
+ */
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    @Override
+    public double sum(final double[][] matrix) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sum'");
+    }
+}
